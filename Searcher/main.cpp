59726d97f436a8e4157f8093592efdae7c6bdd03
artermi <ericcheng230@gmail.com>
2022-01-26 13:24:21 +0800

    main updated

--- Searcher/main.cpp
+++ Searcher/main.cpp
@@ -11,8 +11,21 @@ int main(int argc, char** argv){
 //		dens = atof(argv[2]);
 //	}
 
-	for (double r = 3.2; r < 5; r +=0.1){
-		for(double dens = 0.6; dens < 1.001; dens += 0.05){
+	for (double dens = 0.6; dens < 1.001; dens += 0.05){
+		for(double r = 3; r < 5; r +=0.1){
+
+			char file_name [50];
+			sprintf (file_name, "dns_%03d_r_%04d_c_%04d.dat",
+				(int)((dens+0.00001)*100), (int)((r +0.000001)*1000), (int)((c +0.00001)*1000));
+
+		  FILE *file;
+		  file = fopen(file_name, "r");
+   		if (file) {
+      	fclose(file);
+      	printf("file:'%s' exists\n",file_name);
+      	continue;
+   		}
+
 			printf("Now doing sophisticated PGG with r:%f, density:%f, cost:%f\n",r,dens,c);
 			soph_PGG gameOBJ(r,dens,c);
 			gameOBJ.game(true);
